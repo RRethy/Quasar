fe98e31def589f90d9cbd86d49e29245e1f05209
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2017-12-01 09:48:25 -0500

    Setup accessibility service

--- app/src/main/java/com/bonnetrouge/quasar/MainActivity.java
+++ app/src/main/java/com/bonnetrouge/quasar/MainActivity.java
@@ -1,26 +1,16 @@
 package com.bonnetrouge.quasar;
 
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.bonnetrouge.quasar.Adapters.PagerAdapter;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.view_pager) ViewPager viewPager;
-
-    private android.support.v4.view.PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        this.pagerAdapter = new PagerAdapter(getSupportFragmentManager());
-        this.viewPager.setAdapter(this.pagerAdapter);
     }
 }
